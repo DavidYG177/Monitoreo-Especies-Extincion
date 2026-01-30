be98ce9f82d154f6ab973e93ea88fc8d55400784
David <negritoyepez21@gmail.com>
2026-01-30 18:23:00 -0500

    const

--- src/components/ArchivoConflicto.jsx
+++ src/components/ArchivoConflicto.jsx
@@ -1,1 +1,13 @@
 import React from "react"
+
+const ArchivoConflicto = () => {
+  return (
+    <>
+      <header>
+        <h1>Archivo Conflicto</h1>
+      </header>
+    </>
+  )
+}
+
+export default ArchivoConflicto
